b00d56ffce92a6988cc5eac643933b4b63c47b70
luanamcomin <luana.martinscomin@gmail.com>
2024-09-30 15:53:44 -0300

    página de disciplinas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Login from './pages/login/Login';
 import UserManagement from './pages/user-management/UserManagement';
 import HomePage from './pages/home/HomePage'
-
-import Navbar from './components/navbar/navBar';
+import CourseManagement from './pages/course-menagement/CourseMenagement';
 
 function App() {
   return (
@@ -24,9 +24,13 @@ function App() {
       </nav>   */}
 
       <Routes>
-        <Route path="/" element={<HomePage></HomePage>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/user-management" element={<UserManagement />} />
-      </Routes>
+        <Route path="/course-management" element={<CourseManagement />} />
+{/*     <Route path="/class-management" element={<ClassManagement />} />
+        <Route path="/notification-management" element={<NotificationManagement />} />
+ */}      </Routes>
     </Router>
   );
 }
